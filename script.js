bbd6a1324a6691f0455b012b041bf3caafae185f
Yasar <yasarmansuri16@gmail.com>
2025-06-03 18:14:58 +0530

    some

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ let a = 10;
 let b = 1;
 
 console.log(a + b); // Addition
+
+// Unary Operators
+console.log(++a); // Increment
+console.log(--b); // Decrement
+console.log(a++); // Post-increment
+console.log(b--); // Post-decrement
