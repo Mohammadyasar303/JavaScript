d6b8236ef7d6950441dfda6606dfb558af3012ba
Yasar <yasarmansuri16@gmail.com>
2025-06-03 17:08:00 +0530

    creating a obj

---
+++ first.js
@@ -0,0 +1,23 @@
+// Qs1. Create a const object called “product” to store information shown in the picture.
+
+const product ={
+    name : "Pen",
+    rating : 4.5,
+    price : 270.50,
+    offer : 5
+};
+
+console.log(product);
+
+// Qs2. Create a const object called “profile” to store information shown in the picture.
+
+const profile ={
+    userName : "Yasar",
+    isFollowing : true,
+    posts : 10,
+    followers : 1000,
+    following : 500,
+    bio : "I am a software engineer",
+    isVerified : true
+}
+
