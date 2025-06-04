d14f29389e58e225bad1a516e533dca215880db9
Yasar <yasarmansuri16@gmail.com>
2025-06-04 13:12:22 +0530

    while loop

--- script.js
+++ script.js
@@ -106,13 +106,21 @@
         
     // for loop
         // print "i" 5 times
-        for(let i=1; i<=5; i++){
-            console.log('i');
-        }
+        // for(let i=1; i<=5; i++){
+        //     console.log('i');
+        // }
 
-        // print sum of numbers
-        let sum = 0;
+        // // print sum of numbers
+        // let sum = 0;
 
-        for(let i=1; i<=100; i++){
-            console.log(i ,"+", sum ,"=", sum += i);
+        // for(let i=1; i<=100; i++){
+        //     console.log(i ,"+", sum ,"=", sum += i);
+        // }
+
+    // while loop
+        // print 1 to 5
+        let i = 1;
+        while(i<=5){
+            console.log(i);
+            i++;
         }
