79ee6e42163331a04d63cb42829ea260354ff191
Yasar <yasarmansuri16@gmail.com>
2025-06-04 13:02:55 +0530

    starts loops

--- script.js
+++ script.js
@@ -1,102 +1,107 @@
-// Arithmetic operators
+// // Arithmetic operators
 
-    let a = 10;
-    let b = 1;
+//     let a = 10;
+//     let b = 1;
 
-    console.log(a + b); // Addition
+//     console.log(a + b); // Addition
 
-// Unary Operators
-    console.log(++a); // Increment
-    console.log(--b); // Decrement
-    console.log(a++); // Post-increment
-    console.log(b--); // Post-decrement
-    console.log(++a); // pre increment
-    console.log(--b); // pre decrement
+// // Unary Operators
+//     console.log(++a); // Increment
+//     console.log(--b); // Decrement
+//     console.log(a++); // Post-increment
+//     console.log(b--); // Post-decrement
+//     console.log(++a); // pre increment
+//     console.log(--b); // pre decrement
 
-//  Conditional Statements
+// //  Conditional Statements
  
-// example of if statement
-    let age=18; 
+// // example of if statement
+//     let age=18; 
 
-// ex.1 (if statement)
-    if(age >= 18) {
-        console.log("You are an adult");
-    }
+// // ex.1 (if statement)
+//     if(age >= 18) {
+//         console.log("You are an adult");
+//     }
 
-// ex.2
-    if(age < 18) {
-        console.log("You are a minor");
-    }
+// // ex.2
+//     if(age < 18) {
+//         console.log("You are a minor");
+//     }
 
-// ex.3
+// // ex.3
 
-    let mode = "dark";
-    let color;
+//     let mode = "dark";
+//     let color;
 
-    if(mode === "dark"){
-        color = "black";
-    }
+//     if(mode === "dark"){
+//         color = "black";
+//     }
 
-    if(mode === "light"){
-        color = "white";
-    }
+//     if(mode === "light"){
+//         color = "white";
+//     }
 
-// ex.4 (if-else statement)
+// // ex.4 (if-else statement)
 
-    let num = 10;
+//     let num = 10;
 
-    if(num % 2 == 0){
-        console.log(num , "is a Even number");
-    }else{
-        console.log(num , "is a Odd number");
-    }
+//     if(num % 2 == 0){
+//         console.log(num , "is a Even number");
+//     }else{
+//         console.log(num , "is a Odd number");
+//     }
 
-// ex.5 (else-if statement)
+// // ex.5 (else-if statement)
 
-    let displayMode = "dark";
-    let currentMode; 
+//     let displayMode = "dark";
+//     let currentMode; 
 
-    if (displayMode === "dark") {
-        currentMode = "dark mode";
-    } else if (displayMode === "grey") {
-        currentMode = "grey mode";
-    } else {
-        currentMode = "light mode";
-    }
+//     if (displayMode === "dark") {
+//         currentMode = "dark mode";
+//     } else if (displayMode === "grey") {
+//         currentMode = "grey mode";
+//     } else {
+//         currentMode = "light mode";
+//     }
 
-    console.log("Current Display Mode is :", currentMode);
+//     console.log("Current Display Mode is :", currentMode);
 
-// Ternary Operator
+// // Ternary Operator
 
-    let isLoggedIn = true;
+//     let isLoggedIn = true;
     
-    console.log (isLoggedIn = false ? "Welcome!" : "Try again!");
+//     console.log (isLoggedIn = false ? "Welcome!" : "Try again!");
 
-// Qs1. Get user to input a number using prompt(“Enter a number:”). Check if the number is a multiple of 5 or not.
+// // Qs1. Get user to input a number using prompt(“Enter a number:”). Check if the number is a multiple of 5 or not.
 
-    // let inputNum = prompt("Enter a number :");
+//     // let inputNum = prompt("Enter a number :");
 
-    // if (inputNum % 5 === 0){
-    //     console.log(inputNum , "is a multiple of 5");
-    // } else {
-    //     console.log(inputNum , "is not a multiple of 5");
-    // }
+//     // if (inputNum % 5 === 0){
+//     //     console.log(inputNum , "is a multiple of 5");
+//     // } else {
+//     //     console.log(inputNum , "is not a multiple of 5");
+//     // }
 
-// Qs2. Write a code which can give grades to students according to their scores:
+// // Qs2. Write a code which can give grades to students according to their scores:
 
-    let score = prompt("Enter your Score:");
-    let grade;
+//     let score = prompt("Enter your Score:");
+//     let grade;
 
-    if (score >= 90 && score <= 100) {
-        grade = "A";
-    } else if (score >=70 && score < 90){
-        grade = "B";
-    } else if (score >= 60 && score < 70){
-        grade = "C";
-    } else if (score >= 50 && score < 60){
-        grade = "D";
-    } else {
-        grade = "F";
-    }
+//     if (score >= 90 && score <= 100) {
+//         grade = "A";
+//     } else if (score >=70 && score < 90){
+//         grade = "B";
+//     } else if (score >= 60 && score < 70){
+//         grade = "C";
+//     } else if (score >= 50 && score < 60){
+//         grade = "D";
+//     } else {
+//         grade = "F";
+//     }
 
-    console.log("Your grade is :" , grade);
+//     console.log("Your grade is :" , grade);
+
+
+// Loops :
+        
+    
